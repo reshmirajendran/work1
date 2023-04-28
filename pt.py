3a1eb419f65d6a811b9cbc33d6365677642e5d74
reshmirajendran <129721304+reshmirajendran@users.noreply.github.com>
2023-04-28 12:38:42 +0530

    Add files via upload

---
+++ pt.py
@@ -0,0 +1,10 @@
+from tkinter import*
+
+root=Tk()
+w=Label(root,text="Red Sun",bg="red",fg="White") 
+w.pack(fill=X)
+w=Label(root,text="Green Grass",bg="green",fg="black") 
+w.pack(fill=X)
+w=Label(root,text="blue sky",bg="blue",fg="white")
+w.pack(fill=X)
+mainloop()
